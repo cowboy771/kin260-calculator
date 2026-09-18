1b9477818ac9d666737cfe6c018ef67588ab29f7
cowboy771 <jackhodges7@gmail.com>
2026-09-18 13:27:10 +1000

    u fonts etc

---
+++ kin260-calculator/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Kin260Calculator from './KinCalculator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Kin260Calculator />
+  </React.StrictMode>
+);
